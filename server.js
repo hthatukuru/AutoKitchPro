5433c5a13161747e9fe2c4d716a745ae921fcbd1
Siddhant <sekale@purdue.edu>
2017-02-25 14:53:45 -0600

    Setting index.html for test

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ var express = require('express')
   , app = express()
   , template = require('jade').compileFile(__dirname + '/source/templates/homepage.jade')
 
+const cognitiveServices = require('cognitive-services');
+
 app.use(logger('dev'))
 app.use(express.static(__dirname + '/static'))
 
 app.get('/', function (req, res, next) {
-  try {
-    var html = template({ title: 'Home' })
-    res.send(html)
+try{
+  res.sendFile(__dirname + '/index.html');
   } catch (e) {
     next(e)
   }
