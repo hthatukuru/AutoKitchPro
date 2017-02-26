8d5e338d20bca001527814202e4d744ad8657205
Siddhant <sekale@purdue.edu>
2017-02-25 19:59:03 -0600

    Got image in base64 on node server; need to deploy to blobStorage

--- server.js
+++ server.js
@@ -2,18 +2,28 @@ var express = require('express')
   , logger = require('morgan')
   , app = express()
   , template = require('jade').compileFile(__dirname + '/source/templates/homepage.jade')
+var bodyParser = require('body-parser');
 
 app.use(logger('dev'))
 app.use(express.static(__dirname + '/static'))
 
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencod
+
 app.get('/', function (req, res, next) {
   try {
-    var html = template({ title: 'Home' })
-    res.send(html)
+    res.sendFile(__dirname + '/views/camService.html');
   } catch (e) {
     next(e)
   }
-})
+});
+
+app.post("/picCaptured", function(request, response)
+{
+  //image present in base64 format
+  console.log(request.body);
+});
+
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
