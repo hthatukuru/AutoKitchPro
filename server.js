f526b51bd7e261850c1ae19667926d1e94c21d87
Harika Thatukuru <hthatuku@purdue.edu>
2017-02-25 22:55:58 -0600

    MSFT API working some UI stuff

--- server.js
+++ server.js
@@ -3,13 +3,26 @@ var express = require('express')
   , app = express()
   , template = require('jade').compileFile(__dirname + '/source/templates/homepage.jade')
 
+/* const cognitiveServices = require('cognitive-services');
+
+const emotion = new cognitiveServices.emotion({
+    'API_KEY': '75e5ee1d26c1405eba04783b5abe5f3b'
+});
+
+emotion.emotionRecognition()
+    .then((response) => {
+        console.log('Got response', response);
+    })
+    .catch((err) => {
+        console.error('Encountered error making request:', err);
+    });
+*/
 app.use(logger('dev'))
 app.use(express.static(__dirname + '/static'))
 
 app.get('/', function (req, res, next) {
   try {
-    var html = template({ title: 'Home' })
-    res.send(html)
+    res.sendFile(__dirname + '/views/home.html');
   } catch (e) {
     next(e)
   }
