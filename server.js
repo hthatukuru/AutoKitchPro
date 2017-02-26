ad58ffd3b6fd3a89219a2ed0f511fc2c0c4194b3
Siddhant <sekale@purdue.edu>
2017-02-26 06:51:05 -0600

    Working with Emotion Detection; without UI, need to integrate Harika's UI

--- server.js
+++ server.js
@@ -3,6 +3,20 @@ var express = require('express')
   , app = express()
   , template = require('jade').compileFile(__dirname + '/source/templates/homepage.jade')
 var bodyParser = require('body-parser');
+var azure = require('azure-storage');
+var blobSvc = azure.createBlobService("moodifystorage", "O3fGo+G5qi5P0oakUgRddnMK2qqmyiTePu396fTbvIpJ5fykG6xZtMLoWrZJQh6bsVJ/BExmB9JhAJhmHjf/mQ==");
+
+blobSvc.createContainerIfNotExists('jarviscontainer', function(error, result, response){
+    if(!error){
+      // Container exists and is private
+    }
+});
+
+blobSvc.setContainerAcl('jarviscontainer', null /* signedIdentifiers */, {publicAccessLevel : 'container'} /* publicAccessLevel*/, function(error, result, response){
+  if(!error){
+    // Container access level set to 'container'
+  }
+});
 
 /* const cognitiveServices = require('cognitive-services');
 
@@ -50,15 +64,51 @@ app.get('/', function (req, res, next) {
 
 app.post("/picCaptured", function(request, response)
 {
-  //image present in base64 format
-  console.log(request.body.img);
-  var base64Data = request.body.img.replace(/^data:image\/jpeg;base64,/, "");
+console.log("hello world");
+var rawdata = request.body.img;
+var matches = rawdata.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+var type = matches[1];
+var buffer = new Buffer(matches[2], 'base64');
 
-  require("fs").writeFile("tempImg.jpeg", base64Data, 'base64', function(err) {
-    console.log(err);
-  });
+blobSvc.createBlockBlobFromText('jarviscontainer','tempImg.jpg', buffer, {contentType:type}, function(error, result, response) {
+        if (error) {
+            console.log("screwed", error);
+        }else{
+         console.log("happy", result)
+        }
+    });
+    console.log("Reached ajax");
+
+    // $.ajax({
+    //     url: "https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize?",
+    //     beforeSend: function(xhrObj){
+    //         // Request headers
+    //         xhrObj.setRequestHeader("Content-Type","application/json");
+    //         xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","75e5ee1d26c1405eba04783b5abe5f3b");
+    //     },
+    //     type: "POST",
+    //     // Request body , https://portalstoragewuprod.azureedge.net/emotion/recognition1.jpg
+    //     data: '{ "Url": "https://portalstoragewuprod.azureedge.net/emotion/recognition1.jpg" }'
+    // })
+    // .done(function(data) {
+    //     alert("success");
+    //     if (data.length > 0) {
+    //       alert(data[1].scores.happiness);
+    //     }
+    //     else {
+    //         alert("No faces detected.");
+    //     }
+    // })
+    // .fail(function() {
+    //     console.log("Something messed up");
+    //     alert("error");
+    // });
 });
 
+app.post("/playlist", function(request, response)
+{
+  response.sendFile(__dirname + '/views/playlist.html');
+});
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
