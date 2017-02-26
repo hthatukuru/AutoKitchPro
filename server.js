36c3fbef534ce683aa3ad9121d63c01d243089e8
Siddhant <sekale@purdue.edu>
2017-02-26 07:45:40 -0600

    Comment removal

--- server.js
+++ server.js
@@ -45,13 +45,6 @@ app.get('/spotify-web-api.js', function (req, res, next) {
   }
 });
 
-// app.get('/static/css/homeCSS.css', function (req, res, next){
-//   res.sendFile("static/css/homeCSS.css");
-// }
-
-//app.use(bodyParser.json()); // for parsing application/json
-//app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencod
-
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
@@ -81,31 +74,6 @@ blobSvc.createBlockBlobFromText('jarviscontainer','tempImg.jpg', buffer, {conten
         }
     });
     console.log("Reached ajax");
-
-    // $.ajax({
-    //     url: "https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize?",
-    //     beforeSend: function(xhrObj){
-    //         // Request headers
-    //         xhrObj.setRequestHeader("Content-Type","application/json");
-    //         xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","75e5ee1d26c1405eba04783b5abe5f3b");
-    //     },
-    //     type: "POST",
-    //     // Request body , https://portalstoragewuprod.azureedge.net/emotion/recognition1.jpg
-    //     data: '{ "Url": "https://portalstoragewuprod.azureedge.net/emotion/recognition1.jpg" }'
-    // })
-    // .done(function(data) {
-    //     alert("success");
-    //     if (data.length > 0) {
-    //       alert(data[1].scores.happiness);
-    //     }
-    //     else {
-    //         alert("No faces detected.");
-    //     }
-    // })
-    // .fail(function() {
-    //     console.log("Something messed up");
-    //     alert("error");
-    // });
 });
 /**
  * Generates a random string containing numbers and letters
